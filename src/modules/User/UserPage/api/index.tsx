e4a30ece6266a8d146c64a3f90555bf259a22261
the0ffh <26150100+the0ffh@users.noreply.github.com>
2023-05-21 23:35:30 +0200

    use .env

--- src/modules/User/UserPage/api/index.tsx
+++ src/modules/User/UserPage/api/index.tsx
@@ -1,8 +1,12 @@
 import { Configuration, UserApi } from '../../../../../api';
 
-/*TODO
- *  use EVN VAR!!!!
- */
+console.log(
+  `import.meta.env.API_HOST}${JSON.stringify(import.meta.env, null, 2)}}`,
+);
 export default new UserApi(
-  new Configuration({ basePath: 'http://localhost:3000' }),
+  new Configuration({
+    basePath: `${import.meta.env.VITE_API_HOST}:${
+      import.meta.env.VITE_API_PORT
+    }`,
+  }),
 );
