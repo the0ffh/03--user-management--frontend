fa2bcd7618abeff7f5bf32943273db5279eff7a9
the0ffh <26150100+the0ffh@users.noreply.github.com>
2023-05-22 00:20:24 +0200

    properly refetch users refactor

--- src/modules/User/UserPage/api/index.tsx
+++ src/modules/User/UserPage/api/index.tsx
@@ -1,8 +1,5 @@
 import { Configuration, UserApi } from '../../../../../api';
 
-console.log(
-  `import.meta.env.API_HOST}${JSON.stringify(import.meta.env, null, 2)}}`,
-);
 export default new UserApi(
   new Configuration({
     basePath: `${import.meta.env.VITE_API_HOST}:${
